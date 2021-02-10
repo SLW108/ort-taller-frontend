3437a0e3161828dda513d76aae1a1c26c3dfc99d
Martin <mluzdesign@gmail.com>
2021-02-10 12:53:19 -0300

    add loginUser

--- React/todo-app/src/api/service.js
+++ React/todo-app/src/api/service.js
@@ -12,4 +12,19 @@ const getTodos = () => {
   })
 }
 
-export { getTodos }
+const loginUser = userData => {
+  // Aca hago el fetch y la validación correspondiente
+  // Por ahora data estática
+  const user = {
+    id: 1,
+    name: 'Martín Luz',
+    email: 'martin.luz@gmail.com',
+    token: Math.random()
+  }
+  // cuando lo haga con fetch real al servicio, el propio fetch me devuelve la promesa
+  return new Promise((resolve, reject) => {
+    resolve(user)
+  })
+}
+
+export { getTodos, loginUser }
